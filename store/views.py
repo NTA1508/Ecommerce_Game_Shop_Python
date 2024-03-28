bd0bd2d7169c6e3fb6c16928a527a69847640486
NTA1508 <tuananhbhnd@gmail.com>
2024-03-28 18:04:00 +0700

    first push

---
+++ store/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Product
+
+def home(req):
+    products = Product.objects.all()
+    return render(req, 'home.html', {'products': products})
+
+def about(req):
+    return render(req, 'about.html', {})
