2105560ffdce32e967f052438a848b7f54281a1f
NTA1508 <tuananhbhnd@gmail.com>
2024-03-31 22:02:07 +0700

    Change user profile

--- store/views.py
+++ store/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from .forms import SignUpForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
 
 def home(req):
     products = Product.objects.all()
@@ -42,10 +42,6 @@ def register_user(req):
             form.save()
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
-            # first_name = form.cleaned_data['first_name']
-            # second_name = form.cleaned_data['second_name']
-            # email = form.cleaned_data['email']
-            # Log in user
             user = authenticate(username=username, password=password)
             login(req, user)
             messages.success(req, "You have successfully registered!")
@@ -65,4 +61,40 @@ def category(req, foo):
         return render(req, 'category.html', {'products': products, 'category': category})
     except Category.DoesNotExist:
         messages.error(req, "That category doesn't exist.")
+        return redirect('home')
+
+def update_user(req):
+    if req.user.is_authenticated:
+        current_user = User.objects.get(id=req.user.id)
+        user_form = UpdateUserForm(req.POST or None, instance = current_user)
+        if user_form.is_valid():
+            user_form.save()
+            
+            login(req, current_user)
+            messages.success(req, "User has been updated!")
+            return redirect('home')
+        return render(req, "update_user.html", {'user_form': user_form})
+    else:
+        messages.error(req, "Please Login!")
+        return redirect('home')
+    
+def update_password(req):
+    if req.user.is_authenticated:
+        current_user = req.user
+        if req.method == 'POST':
+            form = ChangePasswordForm(current_user, req.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(req, "Your password has been change!")
+                login(req, current_user)
+                return redirect('home')
+            else:
+                for error in list(form.errors.values()):
+                    messages.error(req, error)
+                    return redirect('update_password')
+        else:
+            form = ChangePasswordForm(current_user)
+            return render(req, "update_password.html", {'form': form})
+    else:
+        messages.error(req, "Please Login!")
         return redirect('home')
