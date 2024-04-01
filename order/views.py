4499457ebac7f8da4cf31458041bcf14f818593b
NTA1508 <tuananhbhnd@gmail.com>
2024-04-02 00:17:42 +0700

    payment

--- order/views.py
+++ order/views.py
@@ -3,7 +3,11 @@ from cart.cart import Cart
 
 # Create your views here.
 def payment_success(req):
-    return render(req, "order/payment_success.html", {})
+    cart = Cart(req)
+    cart_products = cart.get_prods()
+    quantities = cart.get_quants
+    totals = cart.cart_total()
+    return render(req, "order/payment_success.html", {'cart_products': cart_products, 'quantities': quantities, "totals": totals})
 
 def checkout(req):
     cart = Cart(req)
