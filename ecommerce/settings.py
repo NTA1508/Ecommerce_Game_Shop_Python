c31b6c7583953a6c1f1a21a9f9fd1f4e51088761
NTA1508 <tuananhbhnd@gmail.com>
2024-04-01 22:19:43 +0700

    delete payment

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'store',
     'cart',
-    'payment',
 ]
 
 MIDDLEWARE = [
--- payment/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('payment_success', views.payment_success, name = 'payment_success'),
-]   
